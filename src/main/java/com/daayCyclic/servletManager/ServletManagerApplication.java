205e14aea4fc7a4b5f9d2f006982b742ba44d3b1
Youssef <youssef.donadeo@studenti.unicampania.it>
2020-11-23 09:46:47 +0100

    Created the project structure

---
+++ src/main/java/com/daayCyclic/servletManager/ServletManagerApplication.java
@@ -0,0 +1,13 @@
+package com.daayCyclic.servletManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daayCyclic/servletManager/ServletManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daayCyclic.servletManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
