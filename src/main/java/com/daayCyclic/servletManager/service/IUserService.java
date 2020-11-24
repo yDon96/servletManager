924045797bc7b365eb5a38472008ad0209494240
Dominique Barra <d.barra6@studenti.unisa.it>
2020-11-24 20:44:44 +0100

    Implements interfaces for UserRepository and UserService

--- src/main/java/com/daayCyclic/servletManager/service/IUserService.java
+++ src/main/java/com/daayCyclic/servletManager/service/IUserService.java
@@ -1,4 +1,16 @@
 package com.daayCyclic.servletManager.service;
 
+import com.daayCyclic.servletManager.dao.RoleDao;
+import com.daayCyclic.servletManager.dao.UserDao;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IUserService {
+
+    void generateUser(UserDao user);
+    UserDao getUser(int id);
+    List<UserDao> getUsers(Optional<List<String>> rolesList);
+    void assignRoleToUser(UserDao user, RoleDao role);
+
 }
