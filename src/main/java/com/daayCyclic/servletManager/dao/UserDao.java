3f7761cccca97a0cfdd57a118a013cc0b7c803a7
Dominique Barra <d.barra6@studenti.unisa.it>
2020-11-24 19:06:48 +0100

    Add UserDao first implementation

--- src/main/java/com/daayCyclic/servletManager/dao/UserDao.java
+++ src/main/java/com/daayCyclic/servletManager/dao/UserDao.java
@@ -1,7 +1,76 @@
 package com.daayCyclic.servletManager.dao;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-@Entity(name = "user")
+@Entity(name = "Users")
 public class UserDao implements ObjectDao {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+    private String name;
+    private String surname;
+    private LocalDate dateOfBirth;
+    private RoleDao role;
+    private LocalDateTime lastMod;
+    private LocalDateTime timestamp;
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public RoleDao getRole() {
+        return role;
+    }
+
+    public void setRole(RoleDao role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getLastMod() {
+        return lastMod;
+    }
+
+    public void setLastMod(LocalDateTime lastMod) {
+        this.lastMod = lastMod;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
 }
