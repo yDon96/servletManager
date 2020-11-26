003fa30c048e6f455570abc22fad19bc09643c2f
Youssef <you.donadeo@icloud.com>
2020-11-26 19:38:59 +0100

    Define mapper interface methods

--- src/main/java/com/daayCyclic/servletManager/mapper/IDaoToDtoMapper.java
+++ src/main/java/com/daayCyclic/servletManager/mapper/IDaoToDtoMapper.java
@@ -1,4 +1,14 @@
 package com.daayCyclic.servletManager.mapper;
 
+import com.daayCyclic.servletManager.dao.ObjectDao;
+import com.daayCyclic.servletManager.dto.ObjectDto;
+import com.daayCyclic.servletManager.exception.NotValidTypeException;
+
+import java.util.List;
+
 public interface IDaoToDtoMapper {
+
+    ObjectDto convertToDto(ObjectDao objectDao) throws NotValidTypeException;
+
+    List<? extends ObjectDto> convertDaoListToDtoList(List<? extends ObjectDao> daoObjects) throws NotValidTypeException;
 }
--- src/main/java/com/daayCyclic/servletManager/mapper/IDtoToDaoMapper.java
+++ src/main/java/com/daayCyclic/servletManager/mapper/IDtoToDaoMapper.java
@@ -1,4 +1,11 @@
 package com.daayCyclic.servletManager.mapper;
 
+import com.daayCyclic.servletManager.dao.ObjectDao;
+import com.daayCyclic.servletManager.dto.ObjectDto;
+import com.daayCyclic.servletManager.exception.NotValidTypeException;
+
 public interface IDtoToDaoMapper {
+
+    ObjectDao convertToDao(ObjectDto objectDto) throws NotValidTypeException;
+
 }
