774453ddf729c823a0ae6c4c8bc1c4eaabd14b4d
Youssef <you.donadeo@icloud.com>
2020-11-26 19:38:10 +0100

    Give basic implementation to all dao

--- src/main/java/com/daayCyclic/servletManager/dao/RoleDao.java
+++ src/main/java/com/daayCyclic/servletManager/dao/RoleDao.java
@@ -1,7 +1,19 @@
 package com.daayCyclic.servletManager.dao;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity(name = "role")
+@Getter
+@Setter
+@NoArgsConstructor
 public class RoleDao implements ObjectDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
 }
