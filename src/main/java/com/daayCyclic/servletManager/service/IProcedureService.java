e79cdb395b420e9678e1b4bf40003b08cb16c62b
Youssef <you.donadeo@icloud.com>
2020-11-26 20:59:19 +0100

    Define Product service interface

--- src/main/java/com/daayCyclic/servletManager/service/IProcedureService.java
+++ src/main/java/com/daayCyclic/servletManager/service/IProcedureService.java
@@ -1,4 +1,14 @@
 package com.daayCyclic.servletManager.service;
 
+import com.daayCyclic.servletManager.dao.ProcedureDao;
+
+import java.util.List;
+
 public interface IProcedureService {
+
+    void generateProcedure(ProcedureDao procedureDao);
+
+    ProcedureDao getProcedure(Integer procedureId);
+
+    List<ProcedureDao> getProcedures();
 }
