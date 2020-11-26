c000845a418f9298b236b0ab7f4ffa1f7a293bce
Youssef <you.donadeo@icloud.com>
2020-11-26 19:38:34 +0100

    Add new Exception

---
+++ src/main/java/com/daayCyclic/servletManager/exception/NotValidTypeException.java
@@ -0,0 +1,8 @@
+package com.daayCyclic.servletManager.exception;
+
+public class NotValidTypeException extends Exception {
+
+    public NotValidTypeException(String message) {
+        super(message);
+    }
+}
