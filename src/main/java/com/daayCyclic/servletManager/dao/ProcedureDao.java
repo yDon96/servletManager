d99e5d9fa2e2ede3823e1c381712f4438d867f38
Youssef <you.donadeo@icloud.com>
2020-11-26 21:00:48 +0100

    Implemented product dao

--- src/main/java/com/daayCyclic/servletManager/dao/ProcedureDao.java
+++ src/main/java/com/daayCyclic/servletManager/dao/ProcedureDao.java
@@ -3,7 +3,11 @@ package com.daayCyclic.servletManager.dao;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity(name = "activity_procedure")
 @Getter
@@ -14,4 +18,15 @@ public class ProcedureDao implements ObjectDao {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    private String title;
+
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+
+    @UpdateTimestamp
+    private LocalDateTime lastMod;
+
 }
