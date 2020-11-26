2b044a8cfa4f4602ef0e7415209327002b5ecc9e
Youssef <you.donadeo@icloud.com>
2020-11-26 21:00:30 +0100

    Implemented Product dto

--- src/main/java/com/daayCyclic/servletManager/dto/ProcedureDto.java
+++ src/main/java/com/daayCyclic/servletManager/dto/ProcedureDto.java
@@ -1,4 +1,31 @@
 package com.daayCyclic.servletManager.dto;
 
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProcedureDto implements ObjectDto {
+
+    @NotNull
+    public Integer id;
+
+    public String title;
+
+    public String description;
+
+    public ProcedureDto(Integer id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof ProcedureDto
+                && ((ProcedureDto) obj).id.equals(id)
+                && (((ProcedureDto) obj).title == title)
+                && ((ProcedureDto) obj).description.equals(description);
+    }
 }
