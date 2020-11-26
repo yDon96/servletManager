57005b73b290c95474615238473eafc4f81b6e12
Youssef <you.donadeo@icloud.com>
2020-11-26 21:06:10 +0100

    Create Post mapping

--- src/main/java/com/daayCyclic/servletManager/controller/ProcedureController.java
+++ src/main/java/com/daayCyclic/servletManager/controller/ProcedureController.java
@@ -1,7 +1,34 @@
 package com.daayCyclic.servletManager.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.daayCyclic.servletManager.dao.ProcedureDao;
+import com.daayCyclic.servletManager.dto.ProcedureDto;
+import com.daayCyclic.servletManager.exception.NotValidTypeException;
+import com.daayCyclic.servletManager.mapper.IDaoToDtoMapper;
+import com.daayCyclic.servletManager.mapper.IDtoToDaoMapper;
+import com.daayCyclic.servletManager.service.IProcedureService;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class ProcedureController {
+
+    @Autowired
+    private IProcedureService iProcedureService;
+
+    @Autowired
+    @Qualifier("ProcedureMapper")
+    private IDaoToDtoMapper iDaoToDtoMapper;
+
+    @Autowired
+    @Qualifier("ProcedureDtoToDaoMapper")
+    private IDtoToDaoMapper iDtoToDaoMapper;
+
+    @PostMapping(path = "/procedure")
+    public void postProcedure(@RequestBody ProcedureDto procedureDto) throws NotValidTypeException {
+        val procedureDao = (ProcedureDao) iDtoToDaoMapper.convertToDao(procedureDto);
+        iProcedureService.generateProcedure(procedureDao);
+    }
 }
