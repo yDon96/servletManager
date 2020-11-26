84a22601454d5385335a6c683bea1980814a7fcc
Youssef <you.donadeo@icloud.com>
2020-11-26 21:06:58 +0100

    Implemented Procedure service

--- src/main/java/com/daayCyclic/servletManager/service/impl/ProcedureService.java
+++ src/main/java/com/daayCyclic/servletManager/service/impl/ProcedureService.java
@@ -1,8 +1,34 @@
 package com.daayCyclic.servletManager.service.impl;
 
+import com.daayCyclic.servletManager.dao.ProcedureDao;
+import com.daayCyclic.servletManager.repository.IProcedureRepository;
 import com.daayCyclic.servletManager.service.IProcedureService;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProcedureService implements IProcedureService {
+
+    @Autowired
+    private IProcedureRepository iProcedureRepository;
+
+
+    @Override
+    public void generateProcedure(ProcedureDao procedureDao) {
+        iProcedureRepository.save(procedureDao);
+    }
+
+    @Override
+    public ProcedureDao getProcedure(Integer procedureId) {
+        val procedureDao = iProcedureRepository.findById(procedureId);
+        return procedureDao.orElseThrow(() -> new RuntimeException("Not Found."));
+    }
+
+    @Override
+    public List<ProcedureDao> getProcedures() {
+        return iProcedureRepository.findAll();
+    }
 }
