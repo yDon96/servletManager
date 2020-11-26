6b906bbc04d6ddb3894f26b4d3e8c20060253ce3
Dominique Barra <d.barra6@studenti.unisa.it>
2020-11-26 15:17:18 +0100

    Add code structure to UserService

--- src/main/java/com/daayCyclic/servletManager/service/impl/UserService.java
+++ src/main/java/com/daayCyclic/servletManager/service/impl/UserService.java
@@ -1,8 +1,33 @@
 package com.daayCyclic.servletManager.service.impl;
 
+import com.daayCyclic.servletManager.dao.RoleDao;
+import com.daayCyclic.servletManager.dao.UserDao;
+import com.daayCyclic.servletManager.repository.IUserRepository;
 import com.daayCyclic.servletManager.service.IUserService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService implements IUserService{
+
+    @Override
+    public void generateUser(UserDao user) {
+    }
+
+    @Override
+    public UserDao getUser(int id) {
+        return null;
+    }
+
+    @Override
+    public List<UserDao> getUsers(Optional<List<String>> rolesList) {
+        return null;
+    }
+
+    @Override
+    public void assignRoleToUser(UserDao user, RoleDao role) {
+
+    }
 }
