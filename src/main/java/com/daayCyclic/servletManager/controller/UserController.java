acc1a7e6da5d1a8a393e98fbabd202daa1adefd0
Dominique Barra <d.barra6@studenti.unisa.it>
2020-11-27 21:53:56 +0100

    Implement UserController class

--- src/main/java/com/daayCyclic/servletManager/controller/UserController.java
+++ src/main/java/com/daayCyclic/servletManager/controller/UserController.java
@@ -1,52 +1,76 @@
 package com.daayCyclic.servletManager.controller;
 
+import com.daayCyclic.servletManager.dao.RoleDao;
 import com.daayCyclic.servletManager.dao.UserDao;
 import com.daayCyclic.servletManager.dto.UserDto;
+import com.daayCyclic.servletManager.exception.NotValidTypeException;
 import com.daayCyclic.servletManager.mapper.IDaoToDtoMapper;
 import com.daayCyclic.servletManager.mapper.IDtoToDaoMapper;
+import com.daayCyclic.servletManager.service.IRoleService;
 import com.daayCyclic.servletManager.service.IUserService;
+import javassist.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
 
-    @Autowired private IUserService userService;
+    @Autowired @Qualifier(value = "UserService")
+    private IUserService userService;
+
+    @Autowired @Qualifier(value = "RoleService")
+    private IRoleService roleService;
+
+    @Autowired @Qualifier(value = "UserDaoToDtoMapper")
     private IDaoToDtoMapper userDaoToDtoMapper;
+
+    @Autowired @Qualifier(value = "UserDtoToDaoMapper")
     private IDtoToDaoMapper userDtoToDaoMapper;
 
     @PostMapping(path = "/post")
-    public void postUser(UserDto user) {
-        UserDao userDao = userDtoToDaoMapper.convertToDao(user);
+    public void postUser(@RequestBody UserDto user) throws NotValidTypeException {
+        log.info("Start insert/update of a new user into the database: " + user);
+        UserDao userDao = (UserDao) userDtoToDaoMapper.convertToDao(user);
         userService.generateUser(userDao);
+        log.info("Insert/update completed successful");
     }
 
     @GetMapping(path = "/get")
-    public UserDto getUser(int userId) {
+    public UserDto getUser(@RequestParam int userId) throws NotValidTypeException, NotFoundException {
         UserDao searchUser = userService.getUser(userId);
         if (searchUser == null) {
-            System.out.println("Utente non trovato");
-            //throw new
-            //LANCIA ERRORE: "Utente non presente nel db"
+            throw new NotFoundException("Utente non trovato");
         } else {
-            UserDto user = userDaoToDtoMapper.convertToDto(searchUser);
-            return user;
+            return (UserDto) userDaoToDtoMapper.convertToDto(searchUser);
         }
     }
 
     @GetMapping(path = "/get-many")
-    public List<UserDto> getUsers(List<String> roles){
-        return null;
+    public List<UserDto> getUsers(@RequestParam(required = false) List<String> roles) throws NotValidTypeException {
+        log.info("Get Users with roles: " + roles);
+        ArrayList<RoleDao> rolesDao = null;
+        if (roles != null) {
+            rolesDao = new ArrayList<>();
+            for (String role : roles) {
+                rolesDao.add(roleService.getRole(role));
+            }
+        }
+        return (List<UserDto>) userDaoToDtoMapper.convertDaoListToDtoList(userService.getUsers(rolesDao));
     }
 
-    public void assignRoleToUser(String role) {
-
+    public void assignRoleToUser(UserDto user, String role) {
+        if (role != null && user != null) {
+            RoleDao roleDao = roleService.getRole(role);
+            UserDao userDao = userService.getUser(user.getUser_id());
+            userService.assignRoleToUser(userDao, roleDao);
+        }
     }
 
 }
