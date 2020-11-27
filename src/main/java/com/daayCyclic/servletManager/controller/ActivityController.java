e657ddd5339d0a73183b9e65b966cbf11c8588eb
Antonio Annunziata <a.annunziata81@studenti.unisa.it>
2020-11-27 12:25:02 +0100

    initialization of ActivityController

--- src/main/java/com/daayCyclic/servletManager/controller/ActivityController.java
+++ src/main/java/com/daayCyclic/servletManager/controller/ActivityController.java
@@ -1,7 +1,40 @@
 package com.daayCyclic.servletManager.controller;
 
+import com.daayCyclic.servletManager.dto.ActivityDto;
+import com.daayCyclic.servletManager.dto.ObjectDto;
+import com.daayCyclic.servletManager.service.IActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 public class ActivityController {
+
+    @Autowired
+    private IActivityService activityService;
+
+    @PostMapping
+    public void PostActivity(){
+
+    }
+    @GetMapping
+    public ActivityDto GetActivity(){
+        return null;
+
+    }
+    @GetMapping
+    public List<? extends ObjectDto> getActivities(){
+        return null;
+
+    }
+
+
+
+
+
+
+
 }
