250218b5e0b36ec356122cdfaa81c878bab015e5
Antonio Annunziata <a.annunziata81@studenti.unisa.it>
2020-11-27 11:01:40 +0100

    implement ActivityDao

--- src/main/java/com/daayCyclic/servletManager/dao/ActivityDao.java
+++ src/main/java/com/daayCyclic/servletManager/dao/ActivityDao.java
@@ -3,7 +3,7 @@ package com.daayCyclic.servletManager.dao;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import java.time.*;
 import javax.persistence.*;
 
 @Entity(name = "activity")
@@ -15,5 +15,22 @@ public class ActivityDao implements ObjectDao{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    private String description;
+
+    private Integer estimatedTime;
+
+    private boolean isInterruptable;
+
+    private Integer week;
+
+    private ProcedureDao procedure;
+
+    private UserDao maintainer;
+
+    private LocalDateTime lastMod;
+
+    private  LocalDateTime timestamp;
+
 
 }
