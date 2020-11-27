c08931b07e14cdc054f14ab6d3fa2effb82c8148
Antonio Annunziata <a.annunziata81@studenti.unisa.it>
2020-11-27 13:02:07 +0100

    update ActivityController and initialization service Activity

--- src/main/java/com/daayCyclic/servletManager/controller/ActivityController.java
+++ src/main/java/com/daayCyclic/servletManager/controller/ActivityController.java
@@ -2,10 +2,13 @@ package com.daayCyclic.servletManager.controller;
 
 import com.daayCyclic.servletManager.dto.ActivityDto;
 import com.daayCyclic.servletManager.dto.ObjectDto;
+import com.daayCyclic.servletManager.mapper.IDaoToDtoMapper;
+import com.daayCyclic.servletManager.mapper.IDtoToDaoMapper;
 import com.daayCyclic.servletManager.service.IActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
@@ -16,18 +19,39 @@ public class ActivityController {
     @Autowired
     private IActivityService activityService;
 
+    @Autowired
+    private IDaoToDtoMapper iDaoToDtoMapper;
+
+    @Autowired
+    private IDtoToDaoMapper iDtoToDaoMapper;
+
     @PostMapping
     public void PostActivity(){
 
     }
+
+    @PutMapping
+    public void putActivity(){
+
+    }
+
     @GetMapping
     public ActivityDto GetActivity(){
         return null;
 
     }
+
     @GetMapping
     public List<? extends ObjectDto> getActivities(){
         return null;
+
+    }
+    //@?
+    public void assignProcedure(){
+
+    }
+    //@?
+    public void assignMaintainer(){
 
     }
 
--- src/main/java/com/daayCyclic/servletManager/service/IActivityService.java
+++ src/main/java/com/daayCyclic/servletManager/service/IActivityService.java
@@ -1,4 +1,18 @@
 package com.daayCyclic.servletManager.service;
+import com.daayCyclic.servletManager.dao.*;
+import java.util.*;
 
 public interface IActivityService {
+
+    void generateActivity(ActivityDao activityDao);
+
+    ActivityDao ingetActivity(Integer activityId);
+
+    void updateActivity(ActivityDao activityDao);
+
+    List<? extends ObjectDao> getActivities();
+
+    void assignMaintainer(UserDao userDao, ActivityDao activityDao);
+
+    void assignProcedures(ProcedureDao procedureDao, ActivityDao activityDao);
 }
