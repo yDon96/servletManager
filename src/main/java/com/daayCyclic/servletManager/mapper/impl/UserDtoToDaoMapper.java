6485172fe3d1c0e2addc1d7ee2dd15dc5e5a5a18
Dominique Barra <d.barra6@studenti.unisa.it>
2020-11-29 00:56:33 +0100

    Add checks on given ObjectDto (right instance and non-null parameters)

--- src/main/java/com/daayCyclic/servletManager/mapper/impl/UserDtoToDaoMapper.java
+++ src/main/java/com/daayCyclic/servletManager/mapper/impl/UserDtoToDaoMapper.java
@@ -5,12 +5,11 @@ import com.daayCyclic.servletManager.dao.RoleDao;
 import com.daayCyclic.servletManager.dao.UserDao;
 import com.daayCyclic.servletManager.dto.ObjectDto;
 import com.daayCyclic.servletManager.dto.UserDto;
+import com.daayCyclic.servletManager.exception.NotValidTypeException;
 import com.daayCyclic.servletManager.mapper.IDtoToDaoMapper;
 import com.daayCyclic.servletManager.service.IRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.management.relation.Role;
 
 @Component(value = "UserDtoToDaoMapper")
 public class UserDtoToDaoMapper implements IDtoToDaoMapper {
@@ -19,24 +18,39 @@ public class UserDtoToDaoMapper implements IDtoToDaoMapper {
     IRoleService roleService;
 
     @Override
-    public ObjectDao convertToDao(ObjectDto user) {
-        UserDto userDto = (UserDto) user;
-        UserDao newDao = new UserDao();
-        newDao.setUser_id(userDto.getUser_id());
-        newDao.setName(userDto.getName());
-        newDao.setSurname(userDto.getSurname());
-        newDao.setDateOfBirth(userDto.getDateOfBirth());
-        RoleDao tmpRole = roleService.getRole(userDto.getRole());
-        // Start testing
-        if (tmpRole == null) {
-            // !!TESTING da eliminare (nella realtà, deve lanciare un errore se non c'è il ruolo)
-            tmpRole = new RoleDao();
-            tmpRole.setId(0);
-            tmpRole.setRole("STUB");
+    public ObjectDao convertToDao(ObjectDto user) throws NotValidTypeException {
+        if (!(user instanceof UserDto)) {
+            throw new NotValidTypeException("The given object is not an UserDto instance.");
+        } else {
+            UserDto userDto = (UserDto) user;
+            if (!(checkDataIntegrityDto(userDto))) {
+                throw new NotValidTypeException("The given object has one or more 'null' attributes who would violate data integrity.");
+            }
+            UserDao newDao = new UserDao();
+            newDao.setUser_id(userDto.getUser_id());
+            newDao.setName(userDto.getName());
+            newDao.setSurname(userDto.getSurname());
+            newDao.setDateOfBirth(userDto.getDateOfBirth());
+            RoleDao tmpRole = roleService.getRole(userDto.getRole());
+            // Start testing
+            if (tmpRole == null) {
+                // TODO: da eliminare (nella realtà, deve lanciare un errore se non c'è il ruolo)
+                tmpRole = new RoleDao();
+                tmpRole.setId(0);
+                tmpRole.setRole("STUB");
+            }
+            // End testing
+            newDao.setRole(tmpRole);
+            return newDao;
         }
-        // End testing
-        newDao.setRole(tmpRole);
-        return newDao;
+    }
+
+    private boolean checkDataIntegrityDto(UserDto user) {
+        return user != null &&
+                !(user.getUser_id() == null ||
+                  user.getName() == null ||
+                  user.getSurname() == null ||
+                  user.getDateOfBirth() == null);
     }
 
 }
