a74322ef7372e14e5be203f3d6cba56f52a23cd6
Youssef <you.donadeo@icloud.com>
2020-11-28 16:18:38 +0100

    Add log to Rest methods

--- src/main/java/com/daayCyclic/servletManager/controller/ProcedureController.java
+++ src/main/java/com/daayCyclic/servletManager/controller/ProcedureController.java
@@ -2,10 +2,12 @@ package com.daayCyclic.servletManager.controller;
 
 import com.daayCyclic.servletManager.dao.ProcedureDao;
 import com.daayCyclic.servletManager.dto.ProcedureDto;
+import com.daayCyclic.servletManager.exception.DuplicateGenerationException;
 import com.daayCyclic.servletManager.exception.NotValidTypeException;
 import com.daayCyclic.servletManager.mapper.IDaoToDtoMapper;
 import com.daayCyclic.servletManager.mapper.IDtoToDaoMapper;
 import com.daayCyclic.servletManager.service.IProcedureService;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class ProcedureController {
 
@@ -29,22 +32,28 @@ public class ProcedureController {
 
     @PostMapping(path = "/procedure")
     public void postProcedure(@RequestBody ProcedureDto procedureDto) throws NotValidTypeException, DuplicateGenerationException {
+        log.info("[REST] Post procedure with title" + procedureDto.title);
         val procedureDao = (ProcedureDao) iDtoToDaoMapper.convertToDao(procedureDto);
         iProcedureService.generateProcedure(procedureDao);
+        log.debug("[REST] End post procedure");
     }
 
     @GetMapping(path = "/procedure")
     public ProcedureDto getProcedure(@RequestParam Integer id) throws NotValidTypeException {
+        log.info("[REST] Get procedure with id" + id.toString());
         val procedureDao = iProcedureService.getProcedure(id);
         val procedure =(ProcedureDto) iDaoToDtoMapper.convertToDto(procedureDao);
+        log.debug("[REST] End get procedure");
         return procedure;
     }
 
     @GetMapping(path = "/procedures")
     @SuppressWarnings( "unchecked" )
     public List<ProcedureDto> getProcedures() throws NotValidTypeException {
+        log.info("[REST] Get procedures");
         val procedureDao = iProcedureService.getProcedures();
         val procedures =(List<ProcedureDto>) iDaoToDtoMapper.convertDaoListToDtoList(procedureDao);
+        log.debug("[REST] End get procedures");
         return procedures;
     }
 
