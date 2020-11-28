30a2e79d264ddb6f8a38995400d1bdf82708a13f
Youssef <you.donadeo@icloud.com>
2020-11-28 16:30:14 +0100

    Add log to service and mapper

--- src/main/java/com/daayCyclic/servletManager/mapper/impl/ProcedureDtoToDaoMapper.java
+++ src/main/java/com/daayCyclic/servletManager/mapper/impl/ProcedureDtoToDaoMapper.java
@@ -5,23 +5,28 @@ import com.daayCyclic.servletManager.dto.ObjectDto;
 import com.daayCyclic.servletManager.dto.ProcedureDto;
 import com.daayCyclic.servletManager.exception.NotValidTypeException;
 import com.daayCyclic.servletManager.mapper.IDtoToDaoMapper;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component("ProcedureDtoToDaoMapper")
 public class ProcedureDtoToDaoMapper implements IDtoToDaoMapper {
 
 
     @Override
     public ProcedureDao convertToDao(ObjectDto objectDto) throws NotValidTypeException {
+        log.debug("[ProcedureToDtaMapper] Convert procedureDto to dao object");
 
         if (!(objectDto instanceof ProcedureDto)){
             throw new NotValidTypeException("Not Valid Type.");
+            log.error("[ProcedureToDtoMapper] Object to convert is not a ProcedureDto.");
         }
 
         val procedureDto = (ProcedureDto) objectDto;
 
         if (procedureDto.isAllNull()){
+            log.error("[ProcedureToDtoMapper] Object to convert has all empty field.");
             throw new NotValidTypeException("Object to convert has all empty field.");
         }
 
--- src/main/java/com/daayCyclic/servletManager/service/impl/ProcedureService.java
+++ src/main/java/com/daayCyclic/servletManager/service/impl/ProcedureService.java
@@ -5,12 +5,14 @@ import com.daayCyclic.servletManager.exception.DuplicateGenerationException;
 import com.daayCyclic.servletManager.exception.NotFoundException;
 import com.daayCyclic.servletManager.repository.IProcedureRepository;
 import com.daayCyclic.servletManager.service.IProcedureService;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ProcedureService implements IProcedureService {
 
@@ -20,7 +22,9 @@ public class ProcedureService implements IProcedureService {
 
     @Override
     public void generateProcedure(ProcedureDao procedureDao) throws DuplicateGenerationException {
+        log.debug("[ProcedureService] Generating procedure");
         if (procedureExist(procedureDao.getId())){
+            log.error("[ProcedureService] Trying to create a procedure that already exist with id: " + procedureDao.getId().toString());
             throw new DuplicateGenerationException();
         }
         iProcedureRepository.save(procedureDao);
@@ -28,12 +32,14 @@ public class ProcedureService implements IProcedureService {
 
     @Override
     public ProcedureDao getProcedure(Integer procedureId) {
+        log.debug("[ProcedureService] Get procedure with id: " + procedureId.toString());
         val procedureDao = iProcedureRepository.findById(procedureId);
         return procedureDao.orElseThrow(NotFoundException::new);
     }
 
     @Override
     public List<ProcedureDao> getProcedures() {
+        log.debug("[ProcedureService] Get all procedures");
         return iProcedureRepository.findAll();
     }
 
