160f324b4ebd86a0e94181f065a318a930f26305
Dominique Barra <d.barra6@studenti.unisa.it>
2020-11-30 18:20:52 +0100

    Set GenerationType of UserDao to IDENTITY

--- src/main/java/com/daayCyclic/servletManager/dao/RoleDao.java
+++ src/main/java/com/daayCyclic/servletManager/dao/RoleDao.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 public class RoleDao implements ObjectDao {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(unique=true, nullable=false)
