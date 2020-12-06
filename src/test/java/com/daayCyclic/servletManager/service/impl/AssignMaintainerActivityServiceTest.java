b6c70507ef83e3aaa2bad38fafc7c344aa210262
Dominique Barra <d.barra6@studenti.unisa.it>
2020-12-06 17:54:54 +0100

    Add unit testing for assigns (assignProcedure and assignMaintainer)

---
+++ src/test/java/com/daayCyclic/servletManager/service/impl/AssignMaintainerActivityServiceTest.java
@@ -0,0 +1,145 @@
+package com.daayCyclic.servletManager.service.impl;
+
+import com.daayCyclic.servletManager.dao.ActivityDao;
+import com.daayCyclic.servletManager.dao.RoleDao;
+import com.daayCyclic.servletManager.dao.UserDao;
+import com.daayCyclic.servletManager.exception.NotFoundException;
+import com.daayCyclic.servletManager.exception.NotValidTypeException;
+import com.daayCyclic.servletManager.service.IActivityService;
+import com.daayCyclic.servletManager.service.IRoleService;
+import com.daayCyclic.servletManager.service.IUserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDate;
+
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application.yaml")
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class AssignMaintainerActivityServiceTest {
+
+    @Autowired private IActivityService activityService;
+    @Autowired private IUserService userService;
+    @Autowired private IRoleService roleService;
+
+    @BeforeEach
+    void setUp() {
+        addActivities();
+        addRoles();
+        addUsers();
+    }
+
+    @Test
+    void assignMaintainerUserNotPresent() {
+        assertThrows(NotFoundException.class, () -> {
+            RoleDao role = new RoleDao();
+            role.setId(3);
+            role.setName("Maintainer");
+            UserDao user = new UserDao(100, "test", "test", LocalDate.now(), role);
+            activityService.assignMaintainer(user, activityService.getActivity(2));
+        });
+    }
+
+    @Test
+    void assignMaintainerActivityNotPresent() {
+        assertThrows(NotFoundException.class, () -> {
+            ActivityDao activity = new ActivityDao();
+            activity.setId(100);
+            activityService.assignMaintainer(userService.getUser(2), activity);
+        });
+    }
+
+    @Test
+    void assignMaintainerNullUser() {
+        assertThrows(NotValidTypeException.class, () -> {
+            activityService.assignMaintainer(null, activityService.getActivity(2));
+        });
+    }
+
+    @Test
+    void assignMaintainerNullActivity() {
+        assertThrows(NotValidTypeException.class, () -> {
+            activityService.assignMaintainer(userService.getUser(2), null);
+        });
+    }
+
+    @Test
+    void assignMaintainerUserNotAMaintainer() {
+        assertThrows(NotValidTypeException.class, () -> {
+            activityService.assignMaintainer(userService.getUser(1), null);
+        });
+    }
+
+    @Test
+    void assignMaintainerEverythingGood() {
+        UserDao insertedUser = userService.getUser(2);
+        activityService.assignMaintainer(insertedUser, activityService.getActivity(2));
+        ActivityDao retrievedActivity = activityService.getActivity(2);
+        assertEquals(insertedUser, retrievedActivity.getMaintainer());
+    }
+
+    /**
+     * Add some Activities into the database
+     */
+    void addActivities() {
+        for (int i = 1; i < 6; i++) {
+            ActivityDao newActivity = new ActivityDao(
+                    i,
+                    "description" + i,
+                    10 + i,
+                    true,
+                    i * 2,
+                    null,
+                    null);
+            activityService.generateActivity(newActivity);
+        }
+    }
+
+    /**
+     * Add some Users into the database
+     */
+    void addUsers() {
+        for (int i = 1; i < 6; i++) {
+            UserDao newUser = new UserDao(
+                    i,
+                    "name" + i,
+                    "surname" + i,
+                    LocalDate.of(1900, 1, i),
+                    null
+            );
+
+            // Setto Maintainer gli utenti pari e planner gli altri:
+            RoleDao role = new RoleDao();
+            if (i % 2 == 0) {
+                role.setId(3);
+                role.setName("Maintainer");
+            } else {
+                role.setId(2);
+                role.setName("Planner");
+            }
+            newUser.setRole(role);
+
+            userService.generateUser(newUser);
+        }
+    }
+
+    /**
+     * Add some Roles into the database
+     */
+    void addRoles() {
+        String[] roles = {"Admin", "Planner", "Maintainer"};
+        for (String role : roles) {
+            RoleDao roleDao = new RoleDao();
+            roleDao.setName(role);
+            roleService.generateRole(roleDao);
+        }
+    }
+
+}
---
+++ src/test/java/com/daayCyclic/servletManager/service/impl/AssignProcedureActivityServiceTest.java
@@ -0,0 +1,109 @@
+package com.daayCyclic.servletManager.service.impl;
+
+import com.daayCyclic.servletManager.dao.ActivityDao;
+import com.daayCyclic.servletManager.dao.ProcedureDao;
+import com.daayCyclic.servletManager.exception.NotFoundException;
+import com.daayCyclic.servletManager.exception.NotValidTypeException;
+import com.daayCyclic.servletManager.service.IActivityService;
+import com.daayCyclic.servletManager.service.IProcedureService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application.yaml")
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class AssignProcedureActivityServiceTest {
+
+    @Autowired private IActivityService activityService;
+    @Autowired private IProcedureService procedureService;
+
+    @BeforeEach
+    void setUp() {
+        addActivities();
+        addProcedures();
+    }
+
+    @Test
+    void assignProcedureProcedureNotPresent() {
+        assertThrows(NotFoundException.class, () -> {
+            ProcedureDao procedure = new ProcedureDao(100, "test", "test");
+            activityService.assignProcedure(procedure, activityService.getActivity(1));
+        });
+    }
+
+    @Test
+    void assignProcedureActivityNotPresent() {
+        assertThrows(NotFoundException.class, () -> {
+            ActivityDao activity = new ActivityDao();
+            activity.setId(100);
+            activityService.assignProcedure(procedureService.getProcedure(6), activity);
+        });
+    }
+
+    @Test
+    void assignProcedureNullProcedure() {
+        assertThrows(NotValidTypeException.class, () -> {
+           activityService.assignProcedure(null, activityService.getActivity(1));
+        });
+    }
+
+    @Test
+    void assignProcedureNullActivity() {
+        assertThrows(NotValidTypeException.class, () -> {
+            activityService.assignProcedure(procedureService.getProcedure(6), null);
+        });
+    }
+
+    @Test
+    void assignProcedureEverythingGood() {
+        int chosenActivityID = 1;
+        ProcedureDao chosenProcedure = procedureService.getProcedure(6);
+        activityService.assignProcedure(
+                chosenProcedure,
+                activityService.getActivity(chosenActivityID)
+        );
+        ActivityDao retrievedActivity = activityService.getActivity(chosenActivityID);
+        assertEquals(chosenProcedure, retrievedActivity.getProcedure());
+    }
+
+    /**
+     * Add some Activities into the database
+     */
+    void addActivities() {
+        for (int i = 1; i < 6; i++) {
+            ActivityDao newActivity = new ActivityDao(
+                    i,
+                    "description" + i,
+                    10 + i,
+                    true,
+                    i * 2,
+                    null,
+                    null);
+            activityService.generateActivity(newActivity);
+        }
+    }
+
+    /**
+     * Add some Procedures into the database
+     */
+    void addProcedures() {
+        for (int i = 1; i < 6; i++) {
+            ProcedureDao newProcedure = new ProcedureDao(
+                    i,
+                    "procedure" + i,
+                    "description" + i
+            );
+            procedureService.generateProcedure(newProcedure);
+        }
+    }
+
+}
