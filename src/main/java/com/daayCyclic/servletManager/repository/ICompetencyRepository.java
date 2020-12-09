69e53718f1eb42de3f5ec3f7668c1cda981156ee
Dominique Barra <d.barra6@studenti.unisa.it>
2020-12-09 14:27:57 +0100

    Add findByName method in competency repository

--- src/main/java/com/daayCyclic/servletManager/repository/ICompetencyRepository.java
+++ src/main/java/com/daayCyclic/servletManager/repository/ICompetencyRepository.java
@@ -3,5 +3,10 @@ package com.daayCyclic.servletManager.repository;
 import com.daayCyclic.servletManager.dao.CompetencyDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ICompetencyRepository extends JpaRepository<CompetencyDao, Integer> {
+
+    Optional<CompetencyDao> findByName(String name);
+
 }
